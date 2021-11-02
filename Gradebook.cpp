55036824ec1dbfc03b5ac3093fb2649d91707a79
MrRedness <16584585+MrRedness@users.noreply.github.com>
2021-11-02 17:29:37 -0400

    Added basic choice selection

--- Gradebook.cpp
+++ Gradebook.cpp
@@ -2,10 +2,42 @@
 //
 
 #include <iostream>
+#include <thread>
+//#include <chrono>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace std::chrono_literals;
+    std::cout << "Welcome to the Gradebook \n";
+    std::this_thread::sleep_for(1s);
+    std::cout << "Would you like to... \n";
+    std::this_thread::sleep_for(1s);
+    std::cout << "Add a Student? (type add) \n";
+    std::this_thread::sleep_for(1s);
+    std::cout << "Check Student Info? (type check) \n";
+    std::this_thread::sleep_for(1s);
+    std::cout << "Or input grades? (type input) \n";
+
+    bool runningProgram = true;
+    while (runningProgram) {
+        std::string choice;
+        std::cin >> choice;
+        if (choice._Equal("add") || choice._Equal("Add") || choice._Equal("a"))
+        {
+            std::cout << "Ok, let's add a student!\n";
+        }
+        else if (choice._Equal("check") || choice._Equal("Check") || choice._Equal("c"))
+        {
+            std::cout << "Ok, let's check a student's info!\n";
+        }
+        else if (choice._Equal("input") || choice._Equal("Input") || choice._Equal("i"))
+        {
+            std::cout << "Ok, time to input grades!\n";
+        }
+        else {
+            std::cout << "I'm sorry. It seems you entered an invalid choice. Please use \"add\", \"check\", or \"input\".\n";
+        }
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
