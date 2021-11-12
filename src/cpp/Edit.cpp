8b784265cff34ba067b092733a73ac07a175ade6
MrRedness <16584585+MrRedness@users.noreply.github.com>
2021-11-11 21:52:35 -0500

    Added editStudents functionality

--- src/cpp/Edit.cpp
+++ src/cpp/Edit.cpp
@@ -1,9 +1,19 @@
 #include "Edit.hpp"
+#include "MrRed.hpp"
 
 void Edit::editStudents(std::vector<Student> &students)
 {
-    // insert logic to ask user which student they wish to edit
-    editStudent(students[0]);
+    using namespace std::chrono_literals;
+    cOutAndWait("List of students", 1);
+    unsigned int i = 1;
+    for (Student &stu : students)
+    {
+        std::cout << i << " : " << stu.getName() << std::endl;
+        std::this_thread::sleep_for(.5s);
+        i++;
+    }
+    std::cout << "Which student would you like to edit? Enter the number here\n:";
+    editStudent(students[cinInt(1, students.size())]);
 }
 void Edit::editStudent(Student &student)
 {
